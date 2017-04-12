b307bf6ef210dd5dab36fc31f22c04d68683ca6a
zhonggupeng <1812764032@qq.com>
2017-04-12 22:21:04 +0800

    功能加号的改动，暂时动画卡成PPT，解决中

--- app/src/main/java/com/example/asus/shetuan/activity/MainTabActivity.java
+++ app/src/main/java/com/example/asus/shetuan/activity/MainTabActivity.java
@@ -28,7 +28,10 @@ import com.example.asus.shetuan.activity.fragment.FindingFragment;
 import com.example.asus.shetuan.activity.fragment.HomepageFragment;
 import com.example.asus.shetuan.activity.fragment.MeFragment;
 import com.example.asus.shetuan.activity.fragment.MessageFragment;
+import com.example.asus.shetuan.activity.funct.PressActivityActivity;
+import com.example.asus.shetuan.activity.funct.WriteNoticeActivity;
 import com.example.asus.shetuan.databinding.ActivityMainTabBinding;
+import com.example.asus.shetuan.weight.PublishDialog;
 
 public class MainTabActivity extends FragmentActivity {
 
@@ -44,28 +47,17 @@ public class MainTabActivity extends FragmentActivity {
 
     //Tab选项卡的文字
     private String mTextviewArray[] = {"首页", "发现", "  ", "消息", "我的"};
+    private ActivityMainTabBinding binding;
+
+    private PublishDialog publishDialog;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main_tab);
-        ActivityMainTabBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main_tab);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_tab);
 
 //        sharedPreferences = getSharedPreferences("LoginControl", Context.MODE_PRIVATE);
 //        sharedPreferences.edit().putBoolean("isLogin",true).commit();
-
-        /**
-         * 初始化组件
-         */
-
-
-        binding.imageplus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainTabActivity.this,FunctionActivity.class);
-                MainTabActivity.this.startActivity(intent);
-                MainTabActivity.this.overridePendingTransition(R.anim.activity_open,R.anim.activity_close);
-            }
-        });
 
         //实例化布局对象
         layoutInflater = LayoutInflater.from(this);
@@ -87,27 +79,41 @@ public class MainTabActivity extends FragmentActivity {
             //  mTabHost.getTabWidget().getChildAt(i).setBackgroundResource(R.drawable.screen);
 
         }
-        /*
-            假如能够获取到Tab View，就可以设置自己的点击监听，同时覆盖掉了系统的监听器，从而完成自定义点击效果的任务。
-            于是从TabWidget反过来查找获取Tab View的方法。首先TabWidget提供了getChildTabViewAt(int index)方法，可以
-            根据Tab的索引获取到Tab View。然后通过TabHost的getTabWidget可以获取到TabWidget 。得到目标Tab View后，设定自己的OnClickListener，搞定任务。
-
-         */
-//        mTabHost.getTabWidget().getChildTabViewAt(2).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                System.out.println("监听生效了");
-//                Intent intent = new Intent();
-//                intent.setClass(MainTabActivity.this, PlusActivity.class);
-//                startActivity(intent);
-////                overridePendingTransition(R.anim.plusactivityin,0);
-//                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-//
-//
-//            }
-//        });
+        click();
         //解决启动Activy时自动弹出输入法
         this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+    private void click(){
+        binding.imageplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (publishDialog == null){
+                    publishDialog = new PublishDialog(MainTabActivity.this);
+                    publishDialog.setPressactivityClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(MainTabActivity.this, PressActivityActivity.class);
+                            MainTabActivity.this.startActivity(intent);
+                        }
+                    });
+                    publishDialog.setWritenoticeClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(MainTabActivity.this, WriteNoticeActivity.class);
+                            MainTabActivity.this.startActivity(intent);
+                        }
+                    });
+                    publishDialog.setScanningClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(MainTabActivity.this,CaptureActivity.class);
+                            MainTabActivity.this.startActivityForResult(intent,CaptureActivity.REQ_CODE);
+                        }
+                    });
+                }
+                publishDialog.show();
+            }
+        });
     }
 
     /**
